413714ac6c3f48e452e12bd8d3e51b1701f1b110
David Serafine <93729527+dmserafine@users.noreply.github.com>
2022-11-17 17:43:12 -0600

    Add files via upload

--- rules2.py
+++ rules2.py
@@ -8,6 +8,9 @@
 ###		  The URL is for a Global Manager and Global groups
 ###       Place this file in the same directory and run using "python3 rules2.py"
 ###		  This python script requires the csv, requests, json, getpass and pandas modules
+###		  This script will create two files; error.txt and trbl.txt
+###		  error.txt is a file listing all of the group and services objects not found in NSX-T
+###		  trbl.txt is a file containing all datagrams, urls and response codes for API calls
 
 import csv
 import requests
@@ -28,8 +31,12 @@ domainID = 'default'
 
 Headers = {"Content-Type": "application/json"}
 
-getdomainsurl = urlNSX + "/global-manager/api/v1/global-infra/domains/" + domainID + "/groups"
+getgroupsurl = urlNSX + "/global-manager/api/v1/global-infra/domains/" + domainID + "/groups"
 getserviceurl = urlNSX + "/global-manager/api/v1/global-infra/services"
+getsectionurl = urlNSX + "/global-manager/api/v1/global-infra/domains/" + domainID + "/security-policies"
+
+error_file = open("error.txt", "w")
+trbl_file = open("trbl.txt", "w")
 
 # Uses excel data file 
 data_file = pd.read_excel('ruledata.xlsx')
@@ -38,7 +45,7 @@ data_file = pd.read_excel('ruledata.xlsx')
 for row in data_file.itertuples():
 	sequencenum = row.sequence
 	category = row.category
-	section = row.policysection
+	sections = row.policysection
 	name = row.rulename
 	source = row.sources
 	destination = row.destinations
@@ -48,124 +55,189 @@ for row in data_file.itertuples():
 
 # converts multiple entries in source, destination and service field to JSON format list
 	sourcelist = source.strip(" ").split(",")
-	sourcedata = []
+	sourcedata = "any"
 	destinationlist = destination.strip(" ").split(",")
-	destinationdata = []
+	destinationdata = "any"
 	servicelist = service.strip(" ").split(",")
-	servicedata = []
+	servicedata = "any"
 
-# Check domains in the source field to get domainpath if exist
+# check to see if section exists
+	item_found = False
+	getsection = requests.get(getsectionurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
+	sectiondata = getsection.json()
+	for section in sectiondata['results']:
+		sectionName = section['display_name']
+		sectionID = section['id']
+		if section == serviceName:
+			item_found = True
+		else:
+			sectiondata = {
+				    "description": sections,
+				    "display_name": sections,
+				    "category": category
+					}
+			addsectionurl = urlNSX + "/global-manager/api/v1/global-infra/domains/" + domainID + "/security-policies/" + sections
+			addsection = requests.put(addsectionurl, auth=(userNSX,passwordNSX), verify=False, json = sectiondata, headers = Headers)
+			print("Added Section: ",section, file=trbl_file)
+			print(addsection.text, file=trbl_file)
+
+
+# Check groups in the source field to get grouppath if exist
 	for item in sourcelist:
+		item_found = False
 
-# If only one entry exists check if entry is "any" or domain name
+# If only one entry exists check if entry is "any" or group name
 		if len(sourcelist) == 1:
 			if item == "any":
 				sourcedata = "any"
+
+# Verify group name against existing groups in NSX-T and add group as source
 			else:
-				getdomain = requests.get(getdomainsurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
-				domaindata = getdomain.json()
-				for domain in domaindata['results']:
-					domainName = domain['display_name']
-					domainPath = domain['path']
-					while item == domainName:
-						sourcedata = domainpath
+				getgroup = requests.get(getgroupsurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
+				groupdata = getgroup.json()
+				for group in groupdata['results']:
+					groupName = group['display_name']
+					groupPath = group['path']
+					if item == groupName:
+						sourcedata = groupPath
+						item_found = True
 
+# If group does not exist print out rule sequence number and group name							
+				if item_found == False:
+					print("Item not found: ",sequencenum,": ",item, file=error_file)
 
-# If only multiple entries exists check if each entry is valid and combine to array
-		else:
-			getdomain = requests.get(getdomainsurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
-			domaindata = getdomain.json()
-			for domain in domaindata['results']:
-				domainName = domain['display_name']
-				domainPath = domain['path']
-				while item == domainName:
-					sourceentry = domainPath
-					sourcedata.append(sourceentry)
-
-
-# Check domains in the destination field to get domainpath if exist
-	for item in destinationlist:
-
-# If only one entry exists check if entry is "any" or domain name
-		if len(destinationlist) == 1:
-			if item == "any":
-				destinationdata = "any"
-			else:
-				getdomain = requests.get(getdomainsurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
-				domaindata = getdomain.json()
-				for domain in domaindata['results']:
-					domainName = domain['display_name']
-					domainPath = domain['path']
-					while item == domainName:
-						destinationdata = domainpath
 
 # If multiple entries exists check if each entry is valid and combine to array
 		else:
-			getdomain = requests.get(getdomainsurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
-			domaindata = getdomain.json()
-			for domain in domaindata['results']:
-				domainName = domain['display_name']
-				domainPath = domain['path']
-				while item == domainName:
+			if sourcedata == "any":
+				sourcedata = []
+			getgroup = requests.get(getgroupsurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
+			groupdata = getgroup.json()
+			for group in groupdata['results']:
+				groupName = group['display_name']
+				groupPath = group['path']
+				if item == groupName:
+					sourceentry = groupPath
+					sourcedata.append(sourceentry)
+					item_found = True
+
+# If group does not exist print out rule sequence number and group name					
+			if item_found == False:
+				print("Item not found: ",sequencenum,": ",item, file=error_file)
+
+
+# Check groups in the destination field to get grouppath if exist
+	for item in destinationlist:
+		item_found = False
+
+# If only one entry exists check if entry is "any" or group name
+		if len(destinationlist) == 1:
+			if item == "any":
+				destinationdata = "any"
+
+# Verify group name against existing groups in NSX-T and add group as destination				
+			else:
+				getgroup = requests.get(getgroupsurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
+				groupdata = getgroup.json()
+				for group in groupdata['results']:
+					groupName = group['display_name']
+					groupPath = group['path']
+					if item == groupName:
+						destinationdata = groupPath
+						item_found = True
+
+# If group does not exist print out rule sequence number and group name							
+				if item_found == False:
+					print("Item not found: ",sequencenum,": ",item, file=error_file)
+
+# If multiple entries exists check if each entry is valid and combine to array
+		else:
+			if destinationdata =="any":
+				destinationdata = []
+			getgroup = requests.get(getgroupsurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
+			groupdata = getgroup.json()
+			for group in groupdata['results']:
+				groupName = group['display_name']
+				groupPath = group['path']
+				if item == domainName:
 					destinationentry = domainPath
 					destinationdata.append(destinationentry)
+					item_found = True
+
+# If group does not exist print out rule sequence number and group name						
+			if item_found == False:
+				print("Item not found: ",sequencenum,": ",item, file=error_file)
 
 
 # Check services in the service field to get path if exist
 	for item in servicelist:
+		item_found = False
 
 # If only one entry exists check if entry is "any" or service name
 		if len(servicelist) == 1:
 			if item == "any":
-				servicedata = "any"
+				servicedata = ["any"]
+
+# Verify group name against existing groups in NSX-T and add group as service				
 			else:
 				getservice = requests.get(getserviceurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
 				servicejson = getservice.json()
 				for service in servicejson['results']:
 					serviceName = service['display_name']
 					servicePath = service['path']
-					while item == serviceName:
+					if item == serviceName:
 						servicedata = servicePath
+						item_found = True
+
+# If service does not exist print out rule sequence number and service name							
+				if item_found == False:
+					print("Item not found: ",sequencenum,": ",item, file=error_file)
 
 # If multiple entries exists check if each entry is valid and combine to array
 		else:
+			servicedata = []
 			getservice = requests.get(getserviceurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
 			servicejson = getservice.json()
 			for service in servicejson['results']:
 				serviceName = service['display_name']
 				servicePath = service['path']
-				while item == serviceName:
+				if item == serviceName:
 					serviceentry = servicePath
 					servicedata.append(serviceentry)
+					item_found = True
 
+# If service does not exist print out rule sequence number and service name						
+			if item_found == False:
+				print("Item not found: ",sequencenum,": ",item, file=error_file)
+
+	if servicedata == []:
+		servicedata = ["any"]
 
 # build data array for each rule
 	data = {
-		  "description": name,
+		  "action": action.upper(),	
+		  "resource_type": "Rule",
+		  "id": name,
 		  "display_name": name,
-		  "source_groups": [
-		      sourcedata
-		  ],
-		  "logged": "true",
-		  "destination_groups": [
-		      destinationdata
-		  ],
-		  "scope": [
-		      "ANY"
-		  ],
-		  "action": action,
-		  "services": servicedata
+#		  "rule_id": ruleid,
+		  "sequence_number": sequencenum,
+		  "source_groups": [sourcedata],
+		  "destination_groups": [destinationdata],
+		  "services": servicedata,
+		  "profiles": ["any"],
+		  "logged": 'true',		  		  	  
+		  "scope": ["any"],
+		  "disabled": 'false'
 		}
-	print(data)
-
+	print(data, file = trbl_file)
+	
 # URL used to add or create rules
-	addruleurl = userNSX + "/global-manager/api/v1/infra/domains/" + domainID + "/security-policies/" + section + "/rules/" + name
+	addruleurl = urlNSX + "/global-manager/api/v1/global-infra/domains/" + domainID + "/security-policies/" + sections + "/rules/" + name
 
-	print(addruleurl)
-
+	print(addruleurl, file = trbl_file)
+	
 # API call to add rule
-	addrule = requests.put(addruleurl, auth=(userNSX,passwordNSX), verify=False, json = data, headers = Headers)
+	addrule = requests.patch(addruleurl, auth=(userNSX,passwordNSX), verify=False, json = data, headers = Headers)
 
-	print(addrule.text)
-
-
+	print(addrule.text, file = trbl_file)
+	
