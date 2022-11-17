a99ef4614abf4bb47b111ff9d2209f35eb191296
David Serafine <93729527+dmserafine@users.noreply.github.com>
2022-11-17 10:41:29 -0600

    Add files via upload

--- rules2.py
+++ rules2.py
@@ -2,11 +2,11 @@
 ### NOTE: This script uses Python3 to creat a rule per row in a source excel
 ###       The script takes the entire worksheet as a data array
 ###		  from a file named "ruledata.xlsx"
-###		  Column headers should be in order:sequence,category,policysection,rulename,sources,destinations,services,action,applyto
+###		  Column headers should be in order:category,policysection,rulename,sources,destinations,services,action,applyto
 ###		  This script does not use applyto for today - future product enhancement
 ###		  This script places security rules in the default domain 
 ###		  The URL is for a Global Manager and Global groups
-###      	  Place this file in the same directory and run using "python3 rules2.py"
+###       Place this file in the same directory and run using "python3 rules2.py"
 ###		  This python script requires the csv, requests, json, getpass and pandas modules
 
 import csv
@@ -15,16 +15,21 @@ import json
 import getpass
 import pandas as pd
 
-### user input
-urlNSX = input("Enter the FQDN of the NSX manager API? ")
-userNSX = input("Enter the Policy API username for NSX? ")
-passwordNSX = getpass.getpass("Enter the password for the NSX user? ")
+## uncomment for user input
+## urlNSX = input("Enter the FQDN of the NSX manager API? ")
+## userNSX = input("Enter the Policy API username for NSX? ")
+## passwordNSX = getpass.getpass("Enter the password for the NSX user? ")
 
+urlNSX = 'https://nsxtgm.serafine.home'
+userNSX = 'admin'
+passwordNSX ='brg*zwc1vwm3kuc2XNR'
 
 domainID = 'default'
 
 Headers = {"Content-Type": "application/json"}
 
+getdomainsurl = urlNSX + "/global-manager/api/v1/global-infra/domains/" + domainID + "/groups"
+getserviceurl = urlNSX + "/global-manager/api/v1/global-infra/services"
 
 # Uses excel data file 
 data_file = pd.read_excel('ruledata.xlsx')
@@ -52,7 +57,7 @@ for row in data_file.itertuples():
 # Check domains in the source field to get domainpath if exist
 	for item in sourcelist:
 
-# If only one entry exists check if entry is "any" or domain name	
+# If only one entry exists check if entry is "any" or domain name
 		if len(sourcelist) == 1:
 			if item == "any":
 				sourcedata = "any"
@@ -62,30 +67,26 @@ for row in data_file.itertuples():
 				for domain in domaindata['results']:
 					domainName = domain['display_name']
 					domainPath = domain['path']
-					if item == domainName:
+					while item == domainName:
 						sourcedata = domainpath
-					else: 
-						print("No Domain Found For:" + item)
-#						sourcedata = "/global-infra/domains/" + item
 
-# If multiple entries exists check if each entry is valid and combine to array
+
+# If only multiple entries exists check if each entry is valid and combine to array
 		else:
 			getdomain = requests.get(getdomainsurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
 			domaindata = getdomain.json()
 			for domain in domaindata['results']:
 				domainName = domain['display_name']
 				domainPath = domain['path']
-				if item == domainName:
+				while item == domainName:
 					sourceentry = domainPath
 					sourcedata.append(sourceentry)
-				else: 
-					print("No Domain Found For:" + item)
-#					sourceentry = "/global-infra/groups/" + item
-#					sourcedata.append(sourceentry)
 
-# If only one entry exists check if entry is "any" or domain name
+
+# Check domains in the destination field to get domainpath if exist
 	for item in destinationlist:
 
+# If only one entry exists check if entry is "any" or domain name
 		if len(destinationlist) == 1:
 			if item == "any":
 				destinationdata = "any"
@@ -95,11 +96,8 @@ for row in data_file.itertuples():
 				for domain in domaindata['results']:
 					domainName = domain['display_name']
 					domainPath = domain['path']
-					if item == domainName:
+					while item == domainName:
 						destinationdata = domainpath
-					else:
-						print("No Domain Found For:" + item) 
-#						destinationdata = "/global-infra/domains/" + item
 
 # If multiple entries exists check if each entry is valid and combine to array
 		else:
@@ -108,13 +106,10 @@ for row in data_file.itertuples():
 			for domain in domaindata['results']:
 				domainName = domain['display_name']
 				domainPath = domain['path']
-				if item == domainName:
+				while item == domainName:
 					destinationentry = domainPath
 					destinationdata.append(destinationentry)
-				else: 
-					print("No Domain Found For:" + item) 
-#					destinationentry = "/global-infra/groups/" + item
-#					destinationdata.append(destinationentry)
+
 
 # Check services in the service field to get path if exist
 	for item in servicelist:
@@ -125,30 +120,24 @@ for row in data_file.itertuples():
 				servicedata = "any"
 			else:
 				getservice = requests.get(getserviceurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
-				domaindata = getdomain.json()
-				for domain in domaindata['results']:
-					domainName = domain['display_name']
-					domainPath = domain['path']
-					if item == domainName:
-						servicedata = domainpath
-					else:
-						print("No Domain Found For:" + item) 
-#						servicedata = "/global-infra/domains/" + item
+				servicejson = getservice.json()
+				for service in servicejson['results']:
+					serviceName = service['display_name']
+					servicePath = service['path']
+					while item == serviceName:
+						servicedata = servicePath
 
 # If multiple entries exists check if each entry is valid and combine to array
 		else:
 			getservice = requests.get(getserviceurl, auth=(userNSX,passwordNSX), verify=False, headers = Headers)
-			domaindata = getdomain.json()
-			for domain in domaindata['results']:
-				domainName = domain['display_name']
-				domainPath = domain['path']
-				if item == domainName:
-					serviceentry = domainPath
+			servicejson = getservice.json()
+			for service in servicejson['results']:
+				serviceName = service['display_name']
+				servicePath = service['path']
+				while item == serviceName:
+					serviceentry = servicePath
 					servicedata.append(serviceentry)
-				else: 
-					print("No Domain Found For:" + item)
-#					serviceentry = "/global-infra/groups/" + item
-#					servicedata.append(serviceentry)
+
 
 # build data array for each rule
 	data = {
@@ -167,14 +156,14 @@ for row in data_file.itertuples():
 		  "action": action,
 		  "services": servicedata
 		}
-	print(data)	
+	print(data)
 
 # URL used to add or create rules
 	addruleurl = userNSX + "/global-manager/api/v1/infra/domains/" + domainID + "/security-policies/" + section + "/rules/" + name
 
 	print(addruleurl)
 
-# API call to add rule	
+# API call to add rule
 	addrule = requests.put(addruleurl, auth=(userNSX,passwordNSX), verify=False, json = data, headers = Headers)
 
 	print(addrule.text)
